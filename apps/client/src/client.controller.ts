f19d337e833d92253b18776ba86454a507ff64a9
tyler4400 <tyler4400@foxmail.com>
2025-09-15 22:03:46 +0800

    基于event的通信

--- apps/client/src/client.controller.ts
+++ apps/client/src/client.controller.ts
@@ -2,14 +2,20 @@ import { Controller, Get, Inject } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { Observable } from 'rxjs';
 
-@Controller()
+@Controller('test')
 export class ClientController {
   constructor(@Inject('MATH_SERVICE') private client: ClientProxy) {}
 
-  @Get()
+  @Get('message')
   accumulate(): Observable<number> {
     const pattern = { cmd: 'sum' };
     const payload = [1, 2, 3];
     return this.client.send<number>(pattern, { data: payload });
+  }
+
+  @Get('event')
+  publish() {
+    this.client.emit<number>('user_created', { a: 1, b: 2 });
+    return '请查看后台服务terminal';
   }
 }
--- apps/main/src/app.controller.ts
+++ apps/main/src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller } from '@nestjs/common';
 import {
   Ctx,
+  EventPattern,
   MessagePattern,
   NatsContext,
   Payload,
@@ -13,5 +14,22 @@ export class AppController {
     console.log('ctx:', ctx);
     console.log('Payload装饰器也可以接受pipe等', data);
     return (data || []).reduce((a, b) => a + b);
+  }
+
+  @EventPattern('user_created')
+  handleUserCreated(data: Record<string, unknown>) {
+    console.log('基于事件的通信', data);
+  }
+
+  @EventPattern('user_created')
+  handleUserCreated2(
+    @Payload() data: Record<string, unknown>,
+    @Ctx() ctx: NatsContext,
+  ) {
+    console.log(
+      '同一个事件模式注册多个事件处理器，它们都将被自动并行触发',
+      data,
+    );
+    console.log('ctx:', ctx);
   }
 }
